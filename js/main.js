19adeb6cd52be52c31e325f4a19ad6ae9767e090
gayatri <gayatrishimpi7@gmail.com>
2020-09-18 10:09:36 +0530

    removed code

--- js/main.js
+++ js/main.js
@@ -14,35 +14,6 @@ const responsive = {
 }
 
 $(document).ready(function () {
-
-    $nav = $('.nav');
-    $toggleCollapse = $('.toggle-collapse');
-
-    /** click event on toggle menu */
-    $toggleCollapse.click(function () {
-        $nav.toggleClass('collapse');
-    })
-
-    // owl-crousel for blog
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        autoplay: false,
-        autoplayTimeout: 3000,
-        dots: false,
-        nav: true,
-        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
-        responsive: responsive
-    });
-
-
-    // click to scroll top
-    $('.move-up span').click(function () {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 1000);
-    })
-
-    // AOS Instance
     AOS.init();
 
 });
